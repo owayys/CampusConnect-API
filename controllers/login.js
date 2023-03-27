965c0c394f688ae3cbd8b211ca69da887df68e88
Owais Ahsan <owais.ahsan@hotmail.com>
2023-03-27 18:25:47 +0500

    initial commit

--- controllers/login.js
+++ controllers/login.js
@@ -1,9 +1,9 @@
 exports.userLogin = (req, res) => {
     const {username, password} = req.body
-    res.send("Logged")
+    res.send("Logged login")
 }
 
 exports.userSignup = (req, res) => {
-    const {username, password} = req.body
-    res.send("Logged")
+    const {username, email, password} = req.body
+    res.send("Logged signup")
 }
