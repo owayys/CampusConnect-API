9f587a2984ec84f5d45abb1a4e34522c390e0eff
Owais Ahsan <owais.ahsan@hotmail.com>
2023-04-19 22:13:02 +0500

    events api route fix

--- controllers/event.js
+++ controllers/event.js
@@ -1,7 +1,7 @@
 var pool = require('../db/index');
 
 exports.eventGetAll = (req, res) => {
-    pool.query(`SELECT event.event_id, soc_id, event_name, status, event_date, start_time, end_time, location, banner, info, COUNT(events_going.s_id) AS attendees FROM event JOIN events_going ON event.event_id = events_going.event_id GROUP BY event.event_id;`, (err, result) => {
+    pool.query(`SELECT event.event_id, soc_id, event_name, status, event_date, event_time, location, banner, info, COUNT(events_going.s_id) AS attendees FROM event JOIN events_going ON event.event_id = events_going.event_id GROUP BY event.event_id;`, (err, result) => {
         if (err) throw err;
         else {
             res.json(result);
@@ -21,9 +21,9 @@ exports.eventGet = (req, res) => {
 }
 
 exports.eventCreate = (req, res) => {
-    const {soc_id, event_name, event_date, start_time, end_time, location, banner, info} = req.body;
+    const {soc_id, event_name, event_date, event_time, location, banner, info} = req.body;
 
-    pool.query(`INSERT INTO event (soc_id, event_name, status, event_date, start_time, end_time, location, banner, info) VALUES (${soc_id}, ${event_name},'UPCOMING' , ${event_date}, ${start_time}, ${end_time}, ${location}, ${banner}, ${info})`, (err, result) => {
+    pool.query(`INSERT INTO event (soc_id, event_name, status, event_date, event_time, location, banner, info) VALUES (${soc_id}, ${event_name},'UPCOMING' , ${event_date}, ${event_time}, ${location}, ${banner}, ${info})`, (err, result) => {
         if (err) throw err;
         else {
             res.json(result);
@@ -33,9 +33,9 @@ exports.eventCreate = (req, res) => {
 
 exports.eventEdit = (req, res) => {
 
-    const { event_id, soc_id, event_name, event_date, start_time, end_time, location, banner, info} = req.body;
+    const { event_id, soc_id, event_name, event_date, event_time, location, banner, info} = req.body;
 
-    pool.query(`UPDATE event SET soc_id=${soc_id}, event_name=${event_name}, event_date=${event_date}, start_time=${start_time}, end_time=${end_time}, location=${location} banner=${banner}, info=${info} WHERE event_id=${event_id}`, (err, result) => {
+    pool.query(`UPDATE event SET soc_id=${soc_id}, event_name=${event_name}, event_date=${event_date}, event_time=${event_time}, location=${location} banner=${banner}, info=${info} WHERE event_id=${event_id}`, (err, result) => {
         if (err) throw err;
         else {
             res.json(result);
