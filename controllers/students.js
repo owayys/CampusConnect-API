bcd0ecddc15b6c462a544922f87819566d8b8cbd
Owais Ahsan <owais.ahsan@hotmail.com>
2023-04-21 01:21:20 +0500

    student routes fix

--- controllers/students.js
+++ controllers/students.js
@@ -3,7 +3,7 @@ var pool = require('../db/index');
 exports.studentsGetAll = (req, res) => {
     const { s_id } = req.body
 
-    pool.query(`SELECT s_id, s_name, icon FROM students WHERE s_id NOT IN (SELECT s_id FROM students WHERE s_id IN (SELECT s2_id AS friends_list FROM friends WHERE s1_id=${s_id} AND accepted=1 UNION SELECT s1_id AS friends_list FROM friends WHERE s2_id=${s_id})) AND s_id!=${s_id};`, (err, results) => {
+    pool.query(`SELECT s_id, s_name, icon FROM students WHERE s_id NOT IN (SELECT s_id FROM students WHERE s_id IN (SELECT s2_id AS friends_list FROM friends WHERE s1_id=${s_id} UNION SELECT s1_id AS friends_list FROM friends WHERE s2_id=${s_id})) AND s_id!=${s_id};`, (err, results) => {
         if (err) {
             res.json({ error: err })
         }
