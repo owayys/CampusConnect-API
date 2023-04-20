d9c35e1ed1c12120dcc25cae21b1af1337a25ca0
Owais Ahsan <owais.ahsan@hotmail.com>
2023-04-20 21:02:28 +0500

    group routes

--- routers/chatroom.js
+++ routers/chatroom.js
@@ -6,7 +6,7 @@ router.post('/get', chatroomGetAll)
 router.post('/create', chatroomCreate)
 router.post('/member/add', chatroomMemberAdd)
 router.post('/member/remove', chatroomMemberRemove)
-router.get('/message/get', getMessages)
+router.post('/message/get', getMessages)
 router.post('/message/send', sendMessage)
 
 module.exports = router
