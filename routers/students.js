5bd1a60224b6fa51cdd58c56c6c57028fcbf9fb5
Owais Ahsan <owais.ahsan@hotmail.com>
2023-04-21 01:05:21 +0500

    student routes fix

--- routers/students.js
+++ routers/students.js
@@ -2,6 +2,6 @@ const { studentsGetAll } = require('../controllers/students')
 
 const router = require('express').Router()
 
-router.get('/get', studentsGetAll)
+router.post('/get', studentsGetAll)
 
 module.exports = router
